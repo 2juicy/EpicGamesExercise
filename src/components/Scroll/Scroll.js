fd7f4d4eef0f139da33e3a9d106f2cdd77017c25
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-29 19:35:16 -0700

    some progress

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -1,15 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { slide as Menu } from "react-burger-menu";
 import "./Scroll.css";
 
 export default function Scroll({ height }) {
   return (
-    <div className="scrollbar" style={{ height }}>
-      <div className="scroll-text">
-        <p>PAINT YOUR PICTURE</p>
-        <span class="glyphicon glyphicon-chevron-up" />
+    <React.Fragment>
+      <Menu>
+        <a id="home" className="menu-item" href="/">
+          Home
+        </a>
+        <a id="landing" className="menu-item" href="/landing">
+          Landing
+        </a>
+        <a id="bliss" className="menu-item" href="/bliss">
+          Bliss
+        </a>
+      </Menu>
+      <div className="scrollbar" style={{ height }}>
+        <div className="scroll-text">
+          <p>PAINT YOUR PICTURE</p>
+          <a href="#footer" class="glyphicon glyphicon-chevron-up" />
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import content from "../../content.json";
 import Scroll from "../../components/Scroll/Scroll";
 import Intro from "../../components/Intro/Intro";
-import { slide as Menu } from "react-burger-menu";
 import "./Landing.css";
 
 export default function Landing() {
@@ -16,19 +15,9 @@ export default function Landing() {
     );
     return () => window.removeEventListener("resize", resizeListener);
   });
+
   return (
     <div className="landing">
-      <Menu>
-        <a id="home" className="menu-item" href="/">
-          Home
-        </a>
-        <a id="about" className="menu-item" href="/landing">
-          Landing
-        </a>
-        <a id="contact" className="menu-item" href="/contact">
-          Contact
-        </a>
-      </Menu>
       <Scroll height={height} />
       <Intro image={content.intro.header[0]} height={height} />
       <h1>blah blah blah</h1>
