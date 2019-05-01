9ad2e658bf81b7d70f6efbc93eb2a332b76ad08a
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-30 17:44:15 -0700

    updated bliss page

--- src/pages/bliss/Bliss.js
+++ src/pages/bliss/Bliss.js
@@ -1,36 +1,32 @@
 import React, { useState, useEffect } from "react";
 import content from "../../content.json";
 import Scroll from "../../components/Scroll/Scroll";
+import Footer from "../../components/Footer/Footer";
 
 export default function Bliss() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const [height, setHeight] = useState(
     Math.max(document.documentElement.clientHeight)
   );
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const resizeListener = window.addEventListener("resize", resizeLanding);
     return () => window.removeEventListener("resize", resizeListener);
   });
 
-  useEffect(() => {
-    setLoading(false);
-  }, []);
-
   const resizeLanding = () =>
     setHeight(Math.max(document.documentElement.clientHeight));
 
   return (
     <div className="bliss">
-      {loading && (
-        <img
-          className="loading"
-          src={process.env.PUBLIC_URL + "/images/loading.gif"}
-          alt="loading..."
-        />
-      )}
       <Scroll height={height} />
-      <h1>{content.intro.navText}</h1>
+      <Footer
+        footer={content.footer.options}
+        copyright={content.footer.copyright}
+      />
     </div>
   );
 }
