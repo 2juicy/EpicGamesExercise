75fe20ddaecf05bcb3da0b1556960478a882148a
JohnnyC <j0hnnywchiang@gmail.com>
2019-05-01 12:01:30 -0700

    border to side navbar, removed app container

--- src/App.js
+++ src/App.js
@@ -3,29 +3,32 @@ import Home from "./pages/home/Home";
 import Landing from "./pages/landing/Landing";
 import Subpage from "./pages/subpage/Subpage";
 import { Switch, Route } from "react-router";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class App extends Component {
   // This code block is not necessary yet unless I need to make API fetch requests
-  state = {
-    loading: true
-  };
+  // state = {
+  //   loading: true
+  // };
 
-  componentDidMount() {
-    this.setState({ loading: false });
-  }
+  // componentDidMount() {
+  //   this.setState({ loading: false });
+  // }
   // ==================================================================================== //
   render() {
-    if (this.loading) return null;
+    // if (this.loading) return null;
 
     return (
       <div className="App">
-        <div className="app-container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/landing" component={Landing} />
-            <Route exact path="/subpage" component={Subpage} />
-          </Switch>
-        </div>
+        <TransitionGroup>
+          <CSSTransition timeout={450} className="fade">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/landing" component={Landing} />
+              <Route exact path="/subpage" component={Subpage} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
     );
   }
