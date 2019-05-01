10313e6c222ffbd6e4543bb99d222b98f6ee7642
JohnnyC <j0hnnywchiang@gmail.com>
2019-05-01 15:04:24 -0700

    Updates

---
+++ precache-manifest.4f2e695390ebdb743fcc2c77f5f22a95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3fff89ed6ffdf513e8f9f4a293c9d8",
+    "url": "/EpicGamesExercise/index.html"
+  },
+  {
+    "revision": "0da60f2a589e86ec1485",
+    "url": "/EpicGamesExercise/static/css/2.0a305492.chunk.css"
+  },
+  {
+    "revision": "56c2a398806c9139df29",
+    "url": "/EpicGamesExercise/static/css/main.10bd5b82.chunk.css"
+  },
+  {
+    "revision": "0da60f2a589e86ec1485",
+    "url": "/EpicGamesExercise/static/js/2.51b9ba1a.chunk.js"
+  },
+  {
+    "revision": "56c2a398806c9139df29",
+    "url": "/EpicGamesExercise/static/js/main.2dfa3a88.chunk.js"
+  },
+  {
+    "revision": "8bd966aff520068d9163",
+    "url": "/EpicGamesExercise/static/js/runtime~main.2f239cc2.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/EpicGamesExercise/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/EpicGamesExercise/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/EpicGamesExercise/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/EpicGamesExercise/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/EpicGamesExercise/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
